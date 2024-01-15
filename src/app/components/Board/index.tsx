0233bf39e14841ec5acc05775c440892bc0262a0
User <user@Users-MacBook-Air.local>
2024-01-15 14:03:24 +0800

    drag

--- src/app/components/Board/index.tsx
+++ src/app/components/Board/index.tsx
@@ -1,88 +1,86 @@
 "use client";
 
-import React,{ FC } from "react";
-import {Droppable, Draggable} from "react-beautiful-dnd"
+import React, { FC } from "react";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 import { status } from "@/app/utils";
 
-type PropsType ={
-    board:{
-        id: string;
-        title: string;
-        hasBtn: boolean;
-        tasks:{
-            id: string;
-            name: string;
-            priority: string;
-            author: string;
-            isStatus: string;
-        }[];
-    }
-    handleOpen: () => void;
-}
- 
-const Board:FC<PropsType> = ({board}) => {
-    return(
+type PropsType = {
+  board: {
+    id: string;
+    title: string;
+    hasBtn: boolean;
+    tasks: {
+      id: string;
+      name: string;
+      priority: string;
+      author: string;
+      isStatus: string;
+    }[];
+  };
+  handleOpen: () => void;
+};
 
-       <Droppable droppableId={"droppable-" + board.id }>
-        {(provided) => (
-            <div className=" w-1/4 rounded-lg border-2 border-slate-950 h-2/3 bg-slate-100   "
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            >
-                <div className="flex justify-around mt-2 border-b-2 border-slate-950">
-                    <h1 className="text-xl font-semibold ">
-                        {board.title}
-                    </h1>
-                   {board.hasBtn && (
-                     <button className=" flex justify-center items-center w-7 h-7 rounded-full bg-green-600 mb-1 border border-slate-950 "
-                   >
-                     +
-                     </button>
-                   )}
+const Board: FC<PropsType> = ({ board, handleOpen }) => {
+  return (
+    <Droppable droppableId={"droppable-" + board.id}>
+      {(provided) => (
+        <div
+          className=" w-1/4 rounded-lg border-2 border-slate-950 h-2/3 bg-slate-100   "
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+        >
+          <div className="flex justify-around mt-2 border-b-2 border-slate-950">
+            <h1 className="text-xl font-semibold ">{board.title}</h1>
+            {board.hasBtn && (
+              <button className=" flex justify-center items-center w-7 h-7 rounded-full bg-green-600 mb-1 border border-slate-950 ">
+                +
+              </button>
+            )}
+          </div>
+          {board?.tasks.map((task, i) => (
+            <Draggable key={i} draggableId={task.id.toString()} index={i}>
+              {(provided, snapshot) => (
+                <div
+                  className={`card bg-base-100 w-full  shadow-xl my-5 p-5`}
+                  {...provided.dragHandleProps}
+                  {...provided.draggableProps}
+                  ref={provided.innerRef}
+                >
+                  <div className="card-body">
+                    <div className="flex justify-center items-center">
+                      <h2 className="card-title">{task.name}</h2>
+                      <span
+                        className={`badge badge-${
+                          status[task.priority]
+                        } badge-outline ml-5`}
+                      >
+                        {task.priority}
+                      </span>
+                    </div>
+                    <div className="flex flex-col items-start mt-2">
+                      <p className="">
+                        Author: <span className="ml-5 ">Name</span>
+                      </p>
+                      <p className="">
+                        Date: <span>2024-01-11</span>
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                {
-                    board?.tasks.map((task, i)=> (
-                        <Draggable key={i} draggableId={task.id.toString() } index={i}>
-                            {(provided, snapshot)=>(
-                                <div className={`card bg-base-100 w-full  shadow-xl my-5 p-5`}
-                                {...provided.dragHandleProps}
-                                {...provided.draggableProps}
-                                ref={provided.innerRef}
-                                >
-                                    <div className="card-body">
-                                        <div className="flex justify-center items-center">
-                                            <h2 className="card-title">{task.name}</h2>
-                                            <span className={`badge badge-${status[task.priority]} badge-outline ml-5`}>
-                                                {task.priority}
-                                            </span>
-                                        </div>
-                                        <div className="flex flex-col items-start mt-2">
-                                            <p className="">
-                                                Author: <span className="ml-5 ">Name</span>
-                                            </p>
-                                            <p className="">
-                                                Date: <span>2024-01-11</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            )}
-
-                        </Draggable>
-                    ))}
-                    {provided.placeholder}
-            </div>
-        )}
-        
-       </Droppable>
-    )
-}
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>
+  );
+};
 
 export default Board;
 
-
-
-{/* <div className=" w-1/4 rounded-lg border-2 border-slate-950 h-96 bg-slate-100   ">
+{
+  /* <div className=" w-1/4 rounded-lg border-2 border-slate-950 h-96 bg-slate-100   ">
 <div>
     <div className="flex justify-around mt-2 border-b-2 border-slate-950">
         <h1 className="text-xl font-semibold ">To Do</h1>
@@ -91,4 +89,5 @@ export default Board;
         </button>
     </div>
 </div>
-</div> */}
+</div> */
+}
